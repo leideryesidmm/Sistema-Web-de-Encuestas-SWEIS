645f5301fee2884e8690dea958a413b3662faf8c
JheynerLobo <84026784+JheynerLobo@users.noreply.github.com>
2022-11-01 13:51:21 -0500

    Update inicioController.js
    inicioController autenticacion

--- src/controllers/inicioController.js
+++ src/controllers/inicioController.js
@@ -1,20 +1,26 @@
 const controller = {};
-controller.ini =  (req, res) => {
-
-    req.getConnection((error, conn) =>{
-        conn.query('SELECT * FROM encuesta', (err, rows) =>{
-            if (err) {
-                res.json(err);
-            } else {
-                
-                    res.render('inicio', {
-                        encuestas:rows
+    controller.ini =  (req, res) => {
+            var autenticado=req.isAuthenticated()
+            if(autenticado){
+        
+                req.getConnection((error, conn) =>{
+                    conn.query('SELECT * FROM encuesta', (err, rows) =>{
+                        if (err) {
+                            res.json(err);
+                        } else {
+                            
+                            
+                                res.render('inicio', {
+                                    encuestas:rows
+                                })
+                            
+                            //console.log(rows);
+                            
+                        }
                     })
-                
-                //console.log(rows);
-                
-            }
-        })
-    })
-};
-module.exports = controller;
+                })        }else{
+                res.redirect('/')
+        }
+           
+        };
+        module.exports = controller;
